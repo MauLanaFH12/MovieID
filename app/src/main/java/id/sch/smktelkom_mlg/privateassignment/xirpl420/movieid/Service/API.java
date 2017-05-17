ea606d62887f5bc7de881502b18e387058eafb67
MaulanaFH <maulanafadia118@gmail.com>
2017-05-17 11:13:54 +0700

    Add API MovieID

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl420/movieid/Service/API.java
@@ -0,0 +1,10 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl420.movieid.Service;
+
+/**
+ * Created by MaulanaFH on 5/15/2017.
+ */
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "f55bb633c60aa0bc7e346da658237a9d";
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl420/movieid/Trailer.java
@@ -0,0 +1,35 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl420.movieid;
+
+/**
+ * Created by MaulanaFH on 5/15/2017.
+ */
+
+public class Trailer {
+    private String title;
+    private String url;
+    private String type;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
